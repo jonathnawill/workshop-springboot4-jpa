2a4183eca5a9958fbd23df77d73ad7f5791509d8
Jonathan Muniz <Jjoninha11@hotmail.com>
2023-06-28 11:07:25 -0300

    User entity and resource

---
+++ src/main/java/com/jonathnawill/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.jonathnawill.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jonathnawill.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findall(){
+		User u = new User(1L, "Maria", "Maria@gmail.com", "999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+	
+	
+}
