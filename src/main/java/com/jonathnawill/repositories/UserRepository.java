5d01c501b2f6811ed4e643eab7895481a130cece
Jonathan Muniz <Jjoninha11@hotmail.com>
2023-06-28 15:01:59 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/jonathnawill/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.jonathnawill.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jonathnawill.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	
+
+}
